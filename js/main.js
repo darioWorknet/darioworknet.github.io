d538fe8aefe07d06f709f555fe72124e4bf7990c
Dario3Design <dario3design@gmail.com>
2020-12-16 18:28:37 +0100

    CV PDF on profile photo click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	$('.image_overlay h5').html('VIEW PROJECTS');
+	$('#header .image_overlay h5').html('CV');
 	$('.second h5').html('VIEW MORE');
 	
 	breakpoints({
@@ -11,11 +12,14 @@ $(document).ready(function() {
 	breakpoints.on('>small', function() {
 		$('#footer').appendTo($('#header'));
 		$('#footer').addClass('header_content');
+		$('.icons').addClass('move_to_left');
 	});
 
 	breakpoints.on('<=small', function() {
 		$('#footer').insertAfter($('#main'));
 		$('#footer').removeClass($('header_content'));
+		$('.icons').removeClass($('move_to_left'));
+
 	});
 
 });
